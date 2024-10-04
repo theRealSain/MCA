d14d48126a4d831572af9212e801752e25887b6d
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-10-04 14:17:09 +0530

    Delete SEMESTER 3/PYTHON directory

--- SEMESTER 3/PYTHON/Cycle 1/pattern.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter second number: "))
-n3 = int(input("Enter third number: "))
-if(n1 > n2 > n3):
-    print("Decreasing")
-elif(n1 < n2 < n3):
-    print("Increasing")
-else:
-    print("Neither increasing or decreasing")

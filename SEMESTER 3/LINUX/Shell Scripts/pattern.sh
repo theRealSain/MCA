71182820703d7fe122d7835b6d71cee28157dc1d
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-10-04 10:18:48 +0530

    Update pattern.sh

--- SEMESTER 3/LINUX/Shell Scripts/pattern.sh
+++ SEMESTER 3/LINUX/Shell Scripts/pattern.sh
@@ -2,8 +2,6 @@ echo "Enter a pattern to be searched: "
 read pname
 echo "Enter file to be used: "
 read fname
-
 echo "Searching for '$pname' in '$fname'"
 grep "$pname" "$fname"
-
 echo "Selected records shown above"

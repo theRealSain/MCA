1c81c34776a67c0e61e9a988c79b86c6b0d01995
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-17 11:31:54 +0530

    Delete SEMESTER 3/LINUX/Shell Scripts/pattern.sh

--- SEMESTER 3/LINUX/Shell Scripts/pattern.sh
+++
@@ -1,7 +0,0 @@
-echo "Enter a pattern to be searched: "
-read pname
-echo "Enter file to be used: "
-read fname
-echo "Searching for '$pname' in '$fname'"
-grep "$pname" "$fname"
-echo "Selected records shown above"

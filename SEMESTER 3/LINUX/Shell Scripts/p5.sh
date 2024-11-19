cfcd0d0c830306cb6b5b58949509259fa99e5b12
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 07:49:11 +0530

    Update p5.sh

--- SEMESTER 3/LINUX/Shell Scripts/p5.sh
+++ SEMESTER 3/LINUX/Shell Scripts/p5.sh
@@ -1,19 +1,22 @@
-echo "Enter Number: " 
-read num 
-s=0 
-rev=0 
-temp=$num 
- 
-while [ $num -gt 0 ] 
-do 
-s=$(( $num % 10 )) 
-num=$(( $num / 10 )) 
-rev=$( echo ${rev}${s} ) 
-done 
+# Prompt user for input
+echo "Enter Number: "
+read num
 
-if [ $temp -eq $rev ]; 
-then 
-echo "Number is palindrome" 
-else 
-echo "Number is not palindrome" 
+# Initialize variables
+s=0
+rev=0
+temp=$num
+
+# Reverse the number
+while [ $num -gt 0 ]; do
+  s=$((num % 10))          # Get the last digit
+  rev=$((rev * 10 + s))    # Build the reversed number
+  num=$((num / 10))        # Remove the last digit
+done
+
+# Check if the original number and reversed number are the same
+if [ $temp -eq $rev ]; then
+  echo "Number is a palindrome"
+else
+  echo "Number is not a palindrome"
 fi

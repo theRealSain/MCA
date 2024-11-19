3c551c797e45de8f88107a1453c090a2e0c969ad
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 08:04:44 +0530

    Update p6.sh

--- SEMESTER 3/LINUX/Shell Scripts/p6.sh
+++ SEMESTER 3/LINUX/Shell Scripts/p6.sh
@@ -1,12 +1,21 @@
-echo "enter digit number" 
-read num 
-c=1 
- 
-echo "odd numbers" 
+# Prompt the user to enter a number
+echo "Enter digit number:"
+read num
 
-while [ $c -le 5 ] 
-do 
-d= echo $num| cut -c $c 
-echo $d 
-c=$((c+2)) 
+# Initialize counter
+c=1
+
+# Display message
+echo "Odd-positioned digits:"
+
+# Loop to extract and print odd-positioned digits
+while [ $c -le ${#num} ]; do
+  # Extract the character at the c-th position using cut
+  d=$(echo "$num" | cut -c $c)
+  
+  # Print the extracted digit
+  echo $d
+  
+  # Increment counter by 2 to move to the next odd position
+  c=$((c + 2))
 done

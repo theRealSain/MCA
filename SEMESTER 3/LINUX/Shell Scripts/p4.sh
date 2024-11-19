917aa8a11fc98cfacacdfa9099f4098ea6cd173e
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 07:44:50 +0530

    Update p4.sh

--- SEMESTER 3/LINUX/Shell Scripts/p4.sh
+++ SEMESTER 3/LINUX/Shell Scripts/p4.sh
@@ -1,13 +1,17 @@
-echo "Enter Number : "
-read n 
-num=0 
+# Prompt user for input
+echo "Enter Number: "
+read n
 
-while [ $n -gt 0 ] 
-do 
-num=$(expr $num \* 10) 
-k=$(expr $n % 10) 
-num=$(expr $num + $k) 
-n=$(expr $n / 10) 
-done 
- 
+# Initialize reversed number to 0
+num=0
+
+# Reverse the number
+while [ $n -gt 0 ]; do
+  num=$((num * 10))          # Shift digits left in num
+  k=$((n % 10))              # Get the last digit of n
+  num=$((num + k))           # Add the last digit to num
+  n=$((n / 10))              # Remove the last digit from n
+done
+
+# Output the result
 echo "Reversed Number is $num"

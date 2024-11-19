20dc523bb3a0f1be22c32f42e2e8ab1c811fe4ec
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 07:30:42 +0530

    Update p3.sh

--- SEMESTER 3/LINUX/Shell Scripts/p3.sh
+++ SEMESTER 3/LINUX/Shell Scripts/p3.sh
@@ -1,12 +1,16 @@
-echo "enter the number " 
-read n 
-sum=0 
- 
-while [ $n -gt 0 ] 
-do 
-mod=$((n%10)) 
-sum=$((sum+mod)) 
-n=$((n/10)) 
-done 
- 
-echo "sum=$sum"
+# Prompt user for a number
+echo "Enter the number:"
+read n
+
+# Initialize sum variable
+sum=0
+
+# Loop to calculate the sum of digits
+while [ $n -gt 0 ]; do
+  mod=$((n % 10))  # Get the last digit
+  sum=$((sum + mod))  # Add the digit to the sum
+  n=$((n / 10))  # Remove the last digit
+done
+
+# Display the result
+echo "Sum of the digits: $sum"

b0f4fa6383b56ac6de6cff1c1076c56321784a43
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 07:26:58 +0530

    Update p2.sh

--- SEMESTER 3/LINUX/Shell Scripts/p2.sh
+++ SEMESTER 3/LINUX/Shell Scripts/p2.sh
@@ -1,16 +1,21 @@
-echo "Enter length of rectangle:" 
-read length 
-echo "Enter breadth of rectangle:" 
-read breadth 
-echo "Enter radius of circle:" 
-read radius 
+# Prompt for rectangle dimensions
+echo "Enter length of rectangle:"
+read length
+echo "Enter breadth of rectangle:"
+read breadth
 
-rect_area=`echo "$length * $breadth" | bc` 
-rect_perimeter=`echo "2 * ($length + $breadth)" | bc` 
-circle_area=`echo "3.14159 * $radius * $radius" | bc` 
-circle_circumference=`echo "2 * 3.14159 * $radius" | bc` 
+# Prompt for circle radius
+echo "Enter radius of circle:"
+read radius
 
-echo "Rectangle area: $rect_area" 
-echo "Rectangle perimeter: $rect_perimeter" 
-echo "Circle area: $circle_area" 
+# Perform calculations using `bc`
+rect_area=$(echo "$length * $breadth" | bc)
+rect_perimeter=$(echo "2 * ($length + $breadth)" | bc)
+circle_area=$(echo "3.14159 * $radius * $radius" | bc)
+circle_circumference=$(echo "2 * 3.14159 * $radius" | bc)
+
+# Display results
+echo "Rectangle area: $rect_area"
+echo "Rectangle perimeter: $rect_perimeter"
+echo "Circle area: $circle_area"
 echo "Circle circumference: $circle_circumference"

d31c917236b7a754e7c13ce66105f048a04a4d06
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 08:19:08 +0530

    Update p8.sh

--- SEMESTER 3/LINUX/Shell Scripts/p8.sh
+++ SEMESTER 3/LINUX/Shell Scripts/p8.sh
@@ -1,15 +1,17 @@
 echo "Enter the cost price of the item: " 
 read cost_price 
+
 echo "Enter the selling price of the item: " 
 read selling_price 
+
 if [ $cost_price -gt $selling_price ] 
 then 
-loss=$((cost_price - selling_price)) 
-echo "You have incurred a loss of $loss" 
+    loss=$((cost_price - selling_price)) 
+    echo "You have incurred a loss of $loss" 
 elif [ $cost_price -lt $selling_price ] 
 then 
-profit=$((selling_price - cost_price)) 
-echo "You have made a profit of $profit" 
+    profit=$((selling_price - cost_price)) 
+    echo "You have made a profit of $profit" 
 else 
-echo "You have neither made a profit nor incurred a loss." 
+    echo "You have neither made a profit nor incurred a loss." 
 fi

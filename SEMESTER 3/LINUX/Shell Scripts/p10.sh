6540fc28eb0945866fa10d36e5ace471121a78d3
Sain Saburaj <sainsaburajpnc@gmail.com>
2024-11-19 11:28:28 +0530

    Update and rename p10.sh to p10_multiplication.sh

--- SEMESTER 3/LINUX/Shell Scripts/p10.sh
+++
@@ -1,7 +0,0 @@
-echo "enter the number"
-read n
-for i in `seq 1 1 10`
-do
-mul=$((n*i))
-echo "$i*$n=$mul"
-done
---
+++ SEMESTER 3/LINUX/Shell Scripts/p10_multiplication.sh
@@ -0,0 +1,8 @@
+echo "Enter the number"
+read n
+
+for i in `seq 1 1 10`
+do
+    mul=$((n * i))
+    echo "$i * $n = $mul"
+done

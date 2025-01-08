adb6e4fcb26c738aa78071747150cf0bf6e9374e
Sain Saburaj <sainsaburajpnc@gmail.com>
2025-01-09 00:01:53 +0530

    Delete SEMESTER 3/LINUX/Shell Scripts directory

--- SEMESTER 3/LINUX/Shell Scripts/p10_multiplication.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter the number"
-read n
-
-for i in `seq 1 1 10`
-do
-    mul=$((n * i))
-    echo "$i * $n = $mul"
-done
--- SEMESTER 3/LINUX/Shell Scripts/p14_leapyear.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter the year"
-read year
-
-if [ $((year % 400)) -eq 0 ] || ([ $((year % 4)) -eq 0 ] && [ $((year % 100)) -ne 0 ])
-then
-    echo "$year is a leap year"
-else
-    echo "$year is not a leap year"
-fi
